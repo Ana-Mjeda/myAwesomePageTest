3366859279770a4af17eb8247d7cd4f6772396b2
ana.mjeda <anamjeda@gmail.com>
2023-02-09 20:00:20 +0100

    Added pom, and BasePage

---
+++ src/test/java/Pages/BasePage.java
@@ -0,0 +1,35 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BasePage {
+
+    protected WebDriver driver;
+
+    protected WebDriverWait driverWait;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new ChromeDriver();
+        driverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.get("https://vue-demo.daniel-avellaneda.com/");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+}
