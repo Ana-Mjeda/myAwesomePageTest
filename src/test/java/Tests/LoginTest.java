a2bcb7b68253cf204508981911bc6dd8854542e9
ana.mjeda <anamjeda@gmail.com>
2023-02-09 20:43:13 +0100

    added beforeMethod in LoginTest

--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -4,6 +4,7 @@ import Pages.HomePage;
 import Pages.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest{
@@ -18,16 +19,26 @@ public class LoginTest extends BaseTest{
         homePage = new HomePage(driver, driverWait);
         loginPage = new LoginPage(driver, driverWait);
     }
+
+    @BeforeMethod
+    @Override
+    public void beforeMethod() {
+        super.beforeMethod();
+        homePage.clickLogin();
+    }
     @Test
     public void loginURL(){
-        homePage.clickLogin();
         Assert.assertTrue(driver.getCurrentUrl().contains("login"));
     }
 
     @Test
     public void checkInputTypes() {
-        homePage.clickLogin();
         Assert.assertEquals(loginPage.getEmail().getAttribute("type"), "email");
         Assert.assertEquals(loginPage.getPassword().getAttribute("type"), "password");
+    }
+
+    @Test
+    public void checkUserDoesNotExistsError(){
+
     }
 }
