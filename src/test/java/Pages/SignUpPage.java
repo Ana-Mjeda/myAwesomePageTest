73b879032e640f0ab11b107b78d6a6b315a9a51d
ana.mjeda <anamjeda@gmail.com>
2023-02-10 01:02:15 +0100

    added new test in SingUpTest for displaying error when user exists

--- src/test/java/Pages/SignUpPage.java
+++ src/test/java/Pages/SignUpPage.java
@@ -16,6 +16,12 @@ public class SignUpPage extends BasePage{
     @FindBy (id = "confirmPassword")
     private WebElement confirmPassword;
 
+    @FindBy (xpath = "//*[@id=\"app\"]/div/main/div/div[2]/div/div/div[2]/span/form/div/div[5]/button")
+    private WebElement signUpButton;
+
+    @FindBy (xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[3]/div/div/div/div/div[1]/ul/li")
+    private WebElement signUpErrorMessage;
+
     public SignUpPage(WebDriver driver, WebDriverWait driverWait) {
         super(driver, driverWait);
     }
@@ -30,5 +36,17 @@ public class SignUpPage extends BasePage{
 
     public WebElement getPassword() {
         return password;
+    }
+
+    public void fillForm(String name, String email, String password, String confirmPassword){
+        this.name.sendKeys(name);
+        this.email.sendKeys(email);
+        this.password.sendKeys(password);
+        this.confirmPassword.sendKeys(confirmPassword);
+        signUpButton.click();
+    }
+
+    public WebElement getSignUpErrorMessage() {
+        return signUpErrorMessage;
     }
 }
--- src/test/java/Tests/SignUpTest.java
+++ src/test/java/Tests/SignUpTest.java
@@ -1,14 +1,16 @@
 package Tests;
 
 import Pages.SignUpPage;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class SignUpTest extends BaseTest{
+public class SignUpTest extends BaseTest {
 
     private SignUpPage signUpPage;
+
     @BeforeClass
     @Override
     public void beforeClass() {
@@ -28,10 +30,23 @@ public class SignUpTest extends BaseTest{
         Thread.sleep(2000);
         Assert.assertTrue(driver.getCurrentUrl().contains("/signup"));
     }
+
     @Test
-    public void checkInputTypes(){
+    public void checkInputTypes() {
         Assert.assertEquals(signUpPage.getName().getAttribute("type"), "text");
         Assert.assertEquals(signUpPage.getEmail().getAttribute("type"), "email");
         Assert.assertEquals(signUpPage.getPassword().getAttribute("type"), "password");
+    }
+
+    @Test
+    public void displayErrorsWhenUserExists() {
+        String name = "Test Test";
+        String email = "admin@admin.com";
+        String password = "123654";
+        String confirmPassword = "123654";
+        signUpPage.fillForm(name, email, password, confirmPassword);
+        driverWait.until(ExpectedConditions.visibilityOf(signUpPage.getSignUpErrorMessage()));
+        Assert.assertEquals(signUpPage.getSignUpErrorMessage().getText(), "E-mail already exists");
+        Assert.assertTrue(driver.getCurrentUrl().contains("/signup"));
     }
 }
