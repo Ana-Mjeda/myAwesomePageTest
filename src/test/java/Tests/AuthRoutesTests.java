a77cb00890255a467f51df04a6f42ce616bb34a9
ana.mjeda <anamjeda@gmail.com>
2023-02-11 01:25:12 +0100

    added new test unauthorized profile page redirects to login

--- src/test/java/Tests/AuthRoutesTests.java
+++ src/test/java/Tests/AuthRoutesTests.java
@@ -12,4 +12,10 @@ public class AuthRoutesTests extends BaseTest{
         driverWait.until(ExpectedConditions.urlContains("/login"));
         Assert.assertTrue(driver.getCurrentUrl().contains("/login"));
     }
+    @Test
+    public void unauthorizedProfilePageRedirectsToLogin(){
+        driver.get("https://vue-demo.daniel-avellaneda.com/profile");
+        driverWait.until(ExpectedConditions.urlContains("/login"));
+        Assert.assertTrue(driver.getCurrentUrl().contains("/login"));
+    }
 }
